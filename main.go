a40612edd33779f5f69903a9cc8d16a8f8630556
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2024-07-19 17:39:28 +0200

    ci: build multi-platform container images

--- main.go
+++ main.go
@@ -2,37 +2,144 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"strings"
+	"time"
+	"varnish/internal/dagger"
 )
 
 const (
-	version                   = "7.5.0"
-	prometheusExporterVersion = "1.6.1"
-	jqVersion                 = "1.7.1"
+	// Varnish container registry
+	ImageRegistry string = "docker.io"
+	// Varnish container repository
+	ImageRepository string = "library/varnish"
+	// Varnish container tag
+	ImageTag string = "7.5.0"
+	// Varnish container digest
+	ImageDigest string = "sha256:ca2fcecb439f71a3546d025c65857591281a1596c44e8cb816905aa4d1864b61"
+
+	// Varnish Prometheus exporter version
+	PrometheusExporterVersion string = "1.6.1"
+
+	// jq version
+	JqVersion string = "1.7.1"
 )
 
+// Varnish
 type Varnish struct{}
 
-func (v *Varnish) Container() *Container {
-	binaryFileOpts := ContainerWithFileOpts{
-		Owner:       "root:root",
-		Permissions: 0755,
+// Get a Varnish container
+func (varnish *Varnish) Container(
+	ctx context.Context,
+	// Platform to get container for
+	// +optional
+	platform dagger.Platform,
+) (*dagger.Container, error) {
+	prometheusExporter, err := varnish.PrometheusExporter(PrometheusExporterVersion).Overlay(ctx, platform, "")
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Prometheus Exporter overlay: %s", err)
 	}
 
-	exporter := dag.Container().
-		From("registry.access.redhat.com/ubi9/ubi").
-		WithWorkdir("/home").
-		WithEntrypoint([]string{"bash", "-c"}).
-		WithFile("prometheus-exporter.tar.gz", dag.HTTP("https://github.com/jonnenauha/prometheus_varnish_exporter/releases/download/"+prometheusExporterVersion+"/prometheus_varnish_exporter-"+prometheusExporterVersion+".linux-amd64.tar.gz")).
-		WithExec([]string{"tar -xzvf prometheus-exporter.tar.gz"}).
-		File("prometheus_varnish_exporter-" + prometheusExporterVersion + ".linux-amd64/prometheus_varnish_exporter")
+	jq := dag.Jq(JqVersion).Overlay(dagger.JqOverlayOpts{Platform: platform})
+	configurationLoaderScript := dag.CurrentModule().Source().File("varnish-configuration-loader")
 
-	return dag.Container().
-		From("docker.io/varnish:"+version).
-		WithFile("/usr/local/bin/prometheus_varnish_exporter", exporter, binaryFileOpts).
-		WithFile("/usr/local/bin/jq", dag.HTTP("https://github.com/jqlang/jq/releases/download/jq-"+jqVersion+"/jq-linux-amd64"), binaryFileOpts).
-		WithFile("/usr/local/bin/varnish-configuration-loader", dag.CurrentModule().Source().File("varnish-configuration-loader"), binaryFileOpts)
+	container := dag.Container(dagger.ContainerOpts{Platform: platform}).
+		From(ImageRegistry+"/"+ImageRepository+":"+ImageTag+"@"+ImageDigest).
+		WithDirectory("/", prometheusExporter).
+		WithDirectory("/", jq).
+		WithFile("/usr/local/bin/varnish-configuration-loader", configurationLoaderScript, dagger.ContainerWithFileOpts{
+			Owner:       "root:root",
+			Permissions: 0755,
+		})
+
+	return container, nil
 }
 
-func (v *Varnish) Publish(ctx context.Context, registry string, username string, password string, repository string, tag string) (string, error) {
-	return v.Container().WithRegistryAuth(registry, username, dag.SetSecret("registry-password", password)).Publish(ctx, registry+"/"+repository+":"+tag)
+// Varnish Prometheus Exporter
+type VarnishPrometheusExporter struct {
+	// +private
+	Version string
+}
+
+// Varnish Prometheus Exporter constructor
+func (*Varnish) PrometheusExporter(
+	// Varnish Prometheus exporter version to get
+	version string,
+) *VarnishPrometheusExporter {
+	prometheusExporter := &VarnishPrometheusExporter{
+		Version: version,
+	}
+
+	return prometheusExporter
+}
+
+// Get Varnish Prometheus Exporter executable binary
+func (prometheusExporter *VarnishPrometheusExporter) Binary(
+	ctx context.Context,
+	// Platform to get Varnish Prometheus exporter for
+	// +optional
+	platform dagger.Platform,
+) (*dagger.File, error) {
+	if platform == "" {
+		defaultPlatform, err := dag.DefaultPlatform(ctx)
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to get platform: %s", err)
+		}
+
+		platform = defaultPlatform
+	}
+
+	platformElements := strings.Split(string(platform), "/")
+
+	os := platformElements[0]
+	arch := platformElements[1]
+
+	source := dag.Git("https://github.com/jonnenauha/prometheus_varnish_exporter.git").
+		Tag(prometheusExporter.Version)
+
+	commit, err := source.Commit(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get commit hash: %s", err)
+	}
+
+	binary := dag.Golang().
+		RedhatContainer().
+		WithEnvVariable("GOOS", os).
+		WithEnvVariable("GOARCH", arch).
+		WithMountedDirectory(".", source.Tree()).
+		WithExec([]string{"go", "build", "-ldflags", fmt.Sprintf("-X 'main.Version=%s' -X 'main.VersionHash=%s' -X 'main.VersionDate=%s'", prometheusExporter.Version, commit, time.Now().Format("2006-01-02 15:04:05 -07:00"))}).
+		File("prometheus_varnish_exporter")
+
+	return binary, nil
+}
+
+// Get a root filesystem overlay with Varnish Prometheus Exporter
+func (prometheusExporter *VarnishPrometheusExporter) Overlay(
+	ctx context.Context,
+	// Platform to get Varnish Prometheus Exporter for
+	// +optional
+	platform dagger.Platform,
+	// Filesystem prefix under which to install Varnish Prometheus Exporter
+	// +optional
+	prefix string,
+) (*dagger.Directory, error) {
+	if prefix == "" {
+		prefix = "/usr/local"
+	}
+
+	binary, err := prometheusExporter.Binary(ctx, platform)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Varnish Prometheus Exporter binary: %s", err)
+	}
+
+	overlay := dag.Directory().
+		WithDirectory(prefix, dag.Directory().
+			WithFile("bin/prometheus-varnish-exporter", binary),
+		)
+
+	return overlay, nil
 }
